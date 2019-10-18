5e30513a2a2f360c8d25572be95bec188d1d5ec0
Nathan Mousa <nathanmousa1@gmail.com>
2019-10-18 21:40:47 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,7 +2,9 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  name_hash.map do |key, value|
+  compare = 0
+  
+  name_hash.each do |key, value|
     key
   end
 end
